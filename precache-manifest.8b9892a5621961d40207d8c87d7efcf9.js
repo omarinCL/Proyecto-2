ca5fbd304f7bec335e7bce0601e6120bad0ca4ec
Orlando Marín Muñoz <orlando.marin@direcciondecompras.local>
2019-12-06 11:49:33 -0300

    Updates

---
+++ precache-manifest.8b9892a5621961d40207d8c87d7efcf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44eb24b950b014a0a664712c92e1605c",
+    "url": "/proyecto-2/index.html"
+  },
+  {
+    "revision": "62b5d472260b56faf521",
+    "url": "/proyecto-2/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "a2078e9bcbe4f1c0604d",
+    "url": "/proyecto-2/static/css/main.81d3d17f.chunk.css"
+  },
+  {
+    "revision": "62b5d472260b56faf521",
+    "url": "/proyecto-2/static/js/2.9a801b02.chunk.js"
+  },
+  {
+    "revision": "a2078e9bcbe4f1c0604d",
+    "url": "/proyecto-2/static/js/main.23b1b32a.chunk.js"
+  },
+  {
+    "revision": "bff7b2fe1860ef605364",
+    "url": "/proyecto-2/static/js/runtime-main.e573e061.js"
+  }
+]);
